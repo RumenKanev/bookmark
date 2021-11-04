40af426244faafaaef7b94446033a89d63454002
RumenKanev <rkynev@gmail.com>
2021-11-04 11:23:05 +0000

    refactorung with CRUD

--- app.rb
+++ app.rb
@@ -29,5 +29,16 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :"bookmarks/edit"
+  end
+
+  patch '/bookmarks/:id' do
+    p params
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect '/bookmarks'
+  end
+
   run! if app_file == $0
 end
